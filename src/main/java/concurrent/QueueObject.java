9131f6075381ae6663845d97ad7232aa0d382ff8
0704681032 <307517019@qq.com>
2014-11-13 22:04:45 +0800

    如何实现公平的线程调度

---
+++ src/main/java/concurrent/QueueObject.java
@@ -0,0 +1,29 @@
+package main.java.concurrent;
+
+/**
+ * Created by jyy on 2014/11/13.
+ */
+public class QueueObject {
+
+    private boolean isNotified = false;
+
+    public synchronized void doWait() throws InterruptedException {
+
+        while(!isNotified){
+            this.wait();
+        }
+
+        this.isNotified = false;
+
+    }
+
+    public synchronized void doNotify() {
+        this.isNotified = true;
+        this.notify();
+    }
+
+    public boolean equals(Object o) {
+        return this == o;
+    }
+
+}
